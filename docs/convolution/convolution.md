07592e530b26375e4f6833c09bc097e2dc575767
akTARDIGRADE13 <itmtha@gmail.com>
2024-05-08 23:10:08 +0900

    add convolution

---
+++ docs/convolution/convolution.md
@@ -0,0 +1,8 @@
+---
+title: convolution
+documentation_of: //lib/convolution/convolution.hpp
+---
+
+## 概要
+
+NTT friendly な素数に対応した畳み込みのライブラリ
---
+++ verify/convolution/convolution/yosupo_convolution_mod.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+
+#include "lib/convolution/convolution.hpp"
+#include "lib/math/ModInt/mint.hpp"
+
+using mint = akTARDIGRADE13::mint<998244353>;
+
+#include <iostream>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<mint> a(n), b(m);
+    for(int i = 0; i < n; ++i) std::cin >> a[i];
+    for(int i = 0; i < m; ++i) std::cin >> b[i];
+    akTARDIGRADE13::NTT<998244353> convolution;
+    auto c = convolution.multiply(a, b);
+    for(int i = 0; i < n + m - 1; ++i) {
+        std::cout << c[i] << " \n"[i == n + m - 2];
+    }
+
+    return 0;
+}
