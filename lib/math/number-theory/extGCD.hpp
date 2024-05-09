df83c502a0de77dae7bf2a9b44683f2960457bc0
akTARDIGRADE13 <itmtha@gmail.com>
2024-05-09 18:22:15 +0900

    add extGCD

---
+++ lib/math/number-theory/extGCD.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace akTARDIGRADE13 {
+
+template <typename T> T extgcd(T a, T b, T &x, T &y) {
+    T d = a;
+    if(b != 0) {
+        d = extgcd(b, a % b, y, x);
+        y -= (a / b) * x;
+    } else {
+        x = 1;
+        y = 0;
+    }
+    return d;
+}
+
+} // namespace akTARDIGRADE13
